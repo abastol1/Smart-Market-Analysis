c4269ebd053286871886a1c5e7e208be0f954692
Anuj Bastola <abastol1@ramapo.edu>
2019-09-16 09:23:14 -0400

    News Box Template

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,54 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(
+    private http: HttpClient,
+    private platform: Platform
+  ) {
+    this.initializeApp();
+  }
 
+  initializeApp() {
+    this.platform.ready().then(() => {
+      this.getNews();
+    });
+  }
+
+  getNews(){
+    const newsAPIKey = "36f2795f98ae409a811da0ed34415039";
+    const query = "Stock";
+    let newsAPIUrl = "https://newsapi.org/v2/everything?q=" + query + "&apiKey=" + newsAPIKey;
+    console.log("The newsAPIUrl is " + newsAPIUrl);
+    this.http.get(newsAPIUrl)
+      .subscribe(
+        (response) => {
+          let title = response['articles'][0]['title'];
+          let description = response['articles'][0]['description'];
+          let sourceName = response['articles'][0]['source']['name'];
+          let url = response['articles'][0]['url'];
+          console.log("Response is " , response);
+
+          console.log("Length of news: ", response['articles'].length)
+          console.log("Title: ", title);
+          console.log("description: ", description.split(".")[0]);
+          console.log("sourceName: ", sourceName);
+          console.log("url: ", url);
+        },
+        error => {
+          alert("Calling NewsApi Failed");
+        },
+        () => {
+          alert("NewsAPI Call Completed");
+        }
+      )
+  }
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -9,4 +9,18 @@ export class Tab2Page {
 
   constructor() {}
 
+  public names = {
+    "Google" : ["Technology company", "https://seomofo.com/wp-content/uploads/2018/09/new-google-logo-knockoff1.png"],
+    "Jefferies" : ["Investment banking company", "https://master.ctaexpo.com/wp-content/uploads/sites/2/cta-logo19.png"],
+    "Facebook" : ["Social network company", "http://navamassages.com/wp-content/uploads/2018/11/50-Best-Facebook-Logo-Icons-GIF-Transparent-PNG-Images-12.png"],
+    "Amazon" : ["E-commerce company", "https://www.fourjay.org/myphoto/f/127/1270472_amazon-logo-png-transparent-background.png"],
+    "IBM" : ["Computer hardware company", "https://www.impactoneducation.org/wp-content/uploads/2018/06/ibm-logo-png-transparent-background.png"],
+    "HP" : ["Software company", "https://www.pngkey.com/png/detail/74-748780_hp-logo-png-corporate-welln-hewlett-packard-current.png"],
+    "Apple" : ["Technology company", "https://amplab.cs.berkeley.edu/wp-content/uploads/2015/07/logo-apple.png"],
+    "Samsung" : ["Multinational conglomerate company", "http://pluspng.com/img-png/samsung-logo-png-samsung-logo-png-2104.png"],
+    "Tesla": ["Automotive company", "https://pngimg.com/uploads/tesla_logo/tesla_logo_PNG21.png"],
+    "Walmart": ["Retail company", "https://www.edcast.com/corp/wp-content/uploads/2016/11/Walmart-Logo-PNG-Transparent.png"],
+    "JP Morghan & Chase": ["Investment banking company", "https://www.actaturcica.com/wp-content/uploads/2018/06/JPMorgan-Chase-Logo-PNG-Transparent.png"],
+    "Microsoft": ["Technology company", "https://www.freepnglogos.com/uploads/microsoft-logo-hd-26.png"]
+  }
 }
