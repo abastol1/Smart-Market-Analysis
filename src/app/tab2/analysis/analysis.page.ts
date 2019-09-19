ccea64bdef96da948a807b35054358822682e163
Anuj Bastola <abastol1@ramapo.edu>
2019-09-18 23:15:54 -0400

    Modal Detail

--- src/app/tab2/analysis/analysis.page.ts
+++ src/app/tab2/analysis/analysis.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { NavParams, ModalController } from '@ionic/angular';
+import { NavParams, ModalController, Platform } from '@ionic/angular';
+import { Chart } from 'chart.js'
 
 @Component({
   selector: 'app-analysis',
@@ -10,10 +11,47 @@ export class AnalysisPage implements OnInit {
 
   constructor(
     private navParams: NavParams,
-    private modalController: ModalController
-  ) { }
+    private modalController: ModalController,
+    private platform: Platform
+  ) { 
+    this.initializeApp();
+  }
+
+  initializeApp(){
+    this.platform.ready().then(() => {
+      this.useAnotherOneWithWebpack();
+    })
+  }
 
   ngOnInit() {
+  }
+
+  useAnotherOneWithWebpack() {
+    var ctx = (<any>document.getElementById('canvas-chart')).getContext('2d');
+    var chart = new Chart(ctx, {
+        // The type of chart we want to create
+        type: 'pie',
+
+        // The data for our dataset
+        data: {
+            labels: ["Negative", "Neutral", "Positive", ],
+            datasets: [{
+              label: "My First dataset",
+              backgroundColor: [
+                'red',
+                'gray',
+                'green' 
+              ],
+              borderColor: [
+                'white',
+                'white',
+                'white'
+              ],
+              data: [30, 25, 45 ],
+              borderWidth: 2
+            }]
+       }
+    });
   }
 
   closeModal(){
