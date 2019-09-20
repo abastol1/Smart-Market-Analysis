4353cb159a71a8b0064578eab467ca68f874542b
Anuj Bastola <abastol1@ramapo.edu>
2019-09-20 09:46:27 -0400

    Dynamic Chart & Tweets

--- src/app/tab2/analysis/analysis.page.ts
+++ src/app/tab2/analysis/analysis.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NavParams, ModalController, Platform } from '@ionic/angular';
 import { Chart } from 'chart.js'
+import { HttpClient } from '@angular/common/http';
+import { TouchSequence } from 'selenium-webdriver';
 
 @Component({
   selector: 'app-analysis',
@@ -12,21 +14,47 @@ export class AnalysisPage implements OnInit {
   constructor(
     private navParams: NavParams,
     private modalController: ModalController,
-    private platform: Platform
+    private platform: Platform,
+    private http: HttpClient
   ) { 
     this.initializeApp();
   }
 
+  public tweets = []
+
   initializeApp(){
     this.platform.ready().then(() => {
-      this.useAnotherOneWithWebpack();
+      this.getTweetAndSentiment();
+      // this.showAnalysisPieChart();
     })
   }
 
   ngOnInit() {
   }
 
-  useAnotherOneWithWebpack() {
+  getTweetAndSentiment(){
+    this.http.get("http://1afe044f.ngrok.io/")
+    .subscribe(
+      (response) => {
+        console.log("Test Response: " , response);
+        console.log("Negative: ", response['Negative'])
+        console.log("Neutral: ", response['Neutral'])
+        console.log("Positive: ", response['Positive'])
+        this.tweets = response['Tweets']
+        this.showAnalysisPieChart(response['Positive'],response['Neutral'], response['Negative']);
+
+      },
+      error => {
+        alert("Calling Test Flask Call Failed");
+        console.log("Error: ", error);
+      },
+      () => {
+        // alert("Test Flask Call Completed");
+      }
+    )
+  }
+
+  showAnalysisPieChart(positive, neutral, negative) {
     var ctx = (<any>document.getElementById('canvas-chart')).getContext('2d');
     var chart = new Chart(ctx, {
         // The type of chart we want to create
@@ -47,7 +75,7 @@ export class AnalysisPage implements OnInit {
                 'white',
                 'white'
               ],
-              data: [30, 25, 45 ],
+              data: [positive, neutral, negative ],
               borderWidth: 2
             }]
        }
