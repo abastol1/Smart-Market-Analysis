d88a48cab139a69c519c5eed2941d1fe00face00
Anuj Bastola <abastol1@ramapo.edu>
2019-09-22 22:04:53 -0400

    Stock Chart Layout

--- src/app/tab2/analysis/analysis.page.ts
+++ src/app/tab2/analysis/analysis.page.ts
@@ -16,13 +16,14 @@ export class AnalysisPage implements OnInit {
     private modalController: ModalController,
     private platform: Platform,
     private http: HttpClient
-  ) { 
+  ) {
     this.initializeApp();
   }
 
   public tweets = []
+  public dataPoints = []
 
-  initializeApp(){
+  initializeApp() {
     this.platform.ready().then(() => {
       this.getTweetAndSentiment();
       this.adjustGraphs();
@@ -33,72 +34,105 @@ export class AnalysisPage implements OnInit {
   ngOnInit() {
   }
 
-  getTweetAndSentiment(){
+  getTweetAndSentiment() {
     this.http.get("http://127.0.0.1:5000/")
-    .subscribe(
-      (response) => {
-        console.log("Test Response: " , response);
-        console.log("Negative: ", response['Negative'])
-        console.log("Neutral: ", response['Neutral'])
-        console.log("Positive: ", response['Positive'])
-        this.tweets = response['Tweets']
-        this.showAnalysisPieChart(response['Positive'],response['Neutral'], response['Negative']);
+      .subscribe(
+        (response) => {
+          console.log("Test Response: ", response);
+          console.log("Negative: ", response['Negative'])
+          console.log("Neutral: ", response['Neutral'])
+          console.log("Positive: ", response['Positive'])
+          this.tweets = response['Tweets']
+          this.showAnalysisPieChart(response['Positive'], response['Neutral'], response['Negative']);
 
-      },
-      error => {
-        alert("Calling Test Flask Call Failed");
-        console.log("Error: ", error);
-      },
-      () => {
-        // alert("Test Flask Call Completed");
-      }
-    )
+        },
+        error => {
+          alert("Calling Test Flask Call Failed");
+          console.log("Error: ", error);
+        },
+        () => {
+          // alert("Test Flask Call Completed");
+        }
+      )
   }
 
-  adjustGraphs(){
+  // Gets data from Flask Server for plotting graphs
+  adjustGraphs() {
     this.http.get("http://127.0.0.1:5000/graphdetail")
-    .subscribe(
-      (response) => {
-        console.log("Graph DataSets: ", response);
-      },
-      error => {
-        alert("Error While Getting")
-      },
-      () =>{
-        console.log("Graph Detail: API Call Completed");
-      }
-    )
+      .subscribe(
+        (response) => {
+          console.log("Graph DataSets: ", response);
+          this.drawChart(response['Data']);
+        },
+        error => {
+          alert("Error While Getting")
+        },
+        () => {
+          console.log("Graph Detail: API Call Completed");
+        }
+      )
+  }
+
+  // dataArray: array of array[date, stockPrice]
+  drawChart(dataArray) {
+    console.log("In DrawChart: ", dataArray);
+    let data = []
+    for (let i = 0; i < dataArray.length; i++) {
+      let splittedDate = dataArray[i][0].split("/")
+      // data.append({})
+      console.log("Date: ", splittedDate, " Closing Price: ", dataArray[i][1])
+    }
+
+    this.dataPoints = [//array
+      { x: new Date(2012, 1, 1), y: 26 },
+      { x: new Date(2012, 1, 3), y: 38 },
+      { x: new Date(2012, 1, 5), y: 43 },
+      { x: new Date(2012, 1, 7), y: 29 },
+      { x: new Date(2012, 1, 11), y: 41 },
+      { x: new Date(2012, 1, 13), y: 54 },
+      { x: new Date(2012, 1, 20), y: 66 },
+      { x: new Date(2012, 1, 21), y: 60 },
+      { x: new Date(2012, 1, 25), y: 53 },
+      { x: new Date(2012, 1, 27), y: 60 }
+    ]
+    console.log("Datapoints are: ", this.dataPoints);
+
+    var ctx = (<any>document.getElementById('canvas-stockgraph')).getContext('2d');
+    var chart = new Chart(ctx, {
+      type: 'line',
+      data: this.dataPoints,
+    })
   }
 
   showAnalysisPieChart(positive, neutral, negative) {
     var ctx = (<any>document.getElementById('canvas-chart')).getContext('2d');
     var chart = new Chart(ctx, {
-        // The type of chart we want to create
-        type: 'pie',
+      // The type of chart we want to create
+      type: 'pie',
 
-        // The data for our dataset
-        data: {
-            labels: ["Negative", "Neutral", "Positive", ],
-            datasets: [{
-              label: "My First dataset",
-              backgroundColor: [
-                'red',
-                'gray',
-                'green' 
-              ],
-              borderColor: [
-                'white',
-                'white',
-                'white'
-              ],
-              data: [positive, neutral, negative ],
-              borderWidth: 2
-            }]
-       }
+      // The data for our dataset
+      data: {
+        labels: ["Negative", "Neutral", "Positive",],
+        datasets: [{
+          label: "My First dataset",
+          backgroundColor: [
+            'red',
+            'gray',
+            'green'
+          ],
+          borderColor: [
+            'white',
+            'white',
+            'white'
+          ],
+          data: [positive, neutral, negative],
+          borderWidth: 2
+        }]
+      }
     });
   }
 
-  closeModal(){
+  closeModal() {
     this.modalController.dismiss()
   }
 }
